809a80eae12c91cdb637597f474f99d1d379d932
Guilherme Araújo de Oliveira <garaujodeoliveira1@gmail.com>
2021-12-01 17:15:26 -0300

    refactor:reorganize buttons without functions yet

--- main.js
+++ main.js
@@ -1,162 +1,41 @@
-header = document.getElementById("animate")
-header.addEventListener("click", function () {
-  window.open("https://www.github.com/guilhermea23", "_blank")
-})
+document.getElementById("animate").addEventListener("click", function () {
+  if (window.confirm("Deseja ser direcionado ao repositório deste projeto?")) {
+    window.open("https://www.github.com/guilhermea23", "_blank")
+  }
+}
+)
 
 buttonClear = document.getElementById("clear")
-buttonOpenParenthesis = document.getElementById("openParenthesis")
-buttonCloseParenthesis = document.getElementById("closeParenthesis")
-buttonDivision = document.getElementById("division")
-buttonMulti = document.getElementById("multi")
-buttonPlus = document.getElementById("plus")
-buttonLess = document.getElementById("less")
 buttonEqual = document.getElementById("equal")
-button1 = document.getElementById("one")
-button2 = document.getElementById("two")
-button3 = document.getElementById("three")
-button4 = document.getElementById("four")
-button5 = document.getElementById("five")
-button6 = document.getElementById("six")
-button7 = document.getElementById("seven")
-button8 = document.getElementById("eight")
-button9 = document.getElementById("nine")
-button0 = document.getElementById("zero")
-buttonComma = document.getElementById("comma")
-buttonPoint = document.getElementById("point")
-
 
 buttonClear.addEventListener("click", () => {
-  location.reload()
-})
-
-buttonOpenParenthesis.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonCloseParenthesis.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonDivision.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonMulti.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonPlus.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonLess.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button1.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button2.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button3.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button4.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button5.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button6.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button7.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button8.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button9.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-button0.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonComma.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
-})
-
-buttonPoint.addEventListener("click", (e) => {
-  input = document.getElementById("result")
-  input.innerHTML += " "
-  input.innerHTML += e.target.value
-  input.innerHTML += " "
+  if (document.getElementById("operation").textContent === "") {
+    window.alert("A tela já está limpa!")
+  } else {
+    document.getElementById("operation").textContent = ""
+  }
 })
 
 buttonEqual.addEventListener("click", () => {
-  input = document.getElementById("result")
-  formula = input.textContent
-  formula = formula.split(" ").join("")
-  input.textContent = ''
-  input.innerHTML += eval(formula)
+  input = document.getElementById("operation")
+  if (input.textContent === "") {
+    window.alert("Impossível calcular!")
+  } else {
+    formula = input.textContent
+    formula = formula.split(" ").join("")
+    input.textContent = ''
+    input.innerHTML += eval(formula)
+  }
+})
+
+document.getElementById("github").addEventListener("click", () => {
+  window.open("https://www.github.com/guilhermea23", "_blank")
+})
+
+document.getElementById("insta").addEventListener("click", () => {
+  window.open("https://www.instagram.com/guilhermea.23/", "_blank")
+})
+
+document.getElementById("linkedin").addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/guilherme-a-8734a2182/", "_blank")
 })
