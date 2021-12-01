d2d91282f47de49c4db63d3f58563ee49013925d
Guilherme Araújo de Oliveira <garaujodeoliveira1@gmail.com>
2021-12-01 20:02:13 -0300

    updates on functions

--- main.js
+++ main.js
@@ -1,12 +1,146 @@
 document.getElementById("animate").addEventListener("click", function () {
   if (window.confirm("Deseja ser direcionado ao repositório deste projeto?")) {
-    window.open("https://www.github.com/guilhermea23", "_blank")
+    window.open("https://www.github.com/guilhermea23/calculator", "_blank")
   }
 }
 )
 
-buttonClear = document.getElementById("clear")
+function factorial(n) {
+  if (n <= 1) { //caso base
+    return 1
+  } else {
+    factorial = n * fatorial(n - 1)
+    return factorial
+  }
+}
 buttonEqual = document.getElementById("equal")
+buttonSum = document.getElementById("sum")
+buttonMulti = document.getElementById("multi")
+buttonSub = document.getElementById("sub")
+buttonDiv = document.getElementById("div")
+buttonClear = document.getElementById("clear")
+buttonRest = document.getElementById("rest")
+button1 = document.getElementById("b1")
+button2 = document.getElementById("b2")
+button3 = document.getElementById("b3")
+button4 = document.getElementById("b4")
+button5 = document.getElementById("b5")
+button6 = document.getElementById("b6")
+button7 = document.getElementById("b7")
+button8 = document.getElementById("b8")
+button9 = document.getElementById("b9")
+button0 = document.getElementById("b0")
+buttonPoint = document.getElementById("b.")
+buttonOpenC = document.getElementById("openC")
+buttonOpenP = document.getElementById("openP")
+buttonCloseC = document.getElementById("closeC")
+buttonCloseP = document.getElementById("closeP")
+buttonFat = document.getElementById("fat")
+
+buttonSum.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonMulti.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonSub.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonDiv.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button0.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button1.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button2.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button3.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button4.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button5.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button6.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button7.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button8.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+button9.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonRest.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonPoint.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonOpenC.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonOpenP.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonCloseC.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonCloseP.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
+
+buttonFat.addEventListener("click", (e) => {
+  e.preventDefault()
+  document.getElementById("operation").textContent += e.target.value
+})
 
 buttonClear.addEventListener("click", () => {
   if (document.getElementById("operation").textContent === "") {
@@ -17,7 +151,7 @@ buttonClear.addEventListener("click", () => {
 })
 
 buttonEqual.addEventListener("click", () => {
-  input = document.getElementById("operation")
+  let input = document.getElementById("operation")
   if (input.textContent === "") {
     window.alert("Impossível calcular!")
   } else {
